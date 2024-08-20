23974f828f6b2976b855fc13a3de1ec8630eaca4
Gustavo Bueno Aguiar Soares Lima <gbplays641@gmail.com>
2024-08-20 19:43:50 -0300

    Update N2.c

--- N2.c
+++ N2.c
@@ -3,7 +3,7 @@
 
 int main(){
     float hrs, hrsInsec;
-        printf("Por favor insira a quantidade que horas que deseja transformar em segundos: ");
+        printf("Por favor, insira a quantidade que horas que deseja transformar em segundos: ");
         scanf("%f", &hrs);
 
     hrsInsec = hrs * 3600;
