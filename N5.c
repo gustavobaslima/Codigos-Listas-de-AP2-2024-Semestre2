1345b957f5db5a7feaabc11706213bcfe08d2cae
Gustavo Bueno Aguiar Soares Lima <gbplays641@gmail.com>
2024-08-20 19:25:11 -0300

    Update N5.c

--- N5.c
+++ N5.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main(){
 
     int n, fat = 1;
-
+    
     printf("Por favor, insira um valor inteiro e positivo para imprimir o fatorial do mesmo: ");
     scanf("%i", &n);
 
