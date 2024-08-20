807d9d9c7161dc8ae52ef9fa2acea96c62fb605f
Gustavo Bueno Aguiar Soares Lima <gbplays641@gmail.com>
2024-08-20 14:05:38 -0300

    Create N5.c

---
+++ N5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n, fat = 1;
+
+    printf("Por favor, insira um valor inteiro e positivo para imprimir o fatorial do mesmo: ");
+    scanf("%i", &n);
+
+    if(n < 0){
+        printf("Numero invalido paizao nao sobrou nada pro betinha");
+
+    }else{
+            for(int i = 1; i<=n; i++){
+            fat *=i;
+        }
+    printf("O fatorial deste numero eh: %i", fat);
+
+}
+}
